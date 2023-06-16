ba74fca4d6ad6b32fee6863465319e624678fcc7
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-06-16 13:17:56 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/cloud-tasks-queue-iam-policy/index.ts
+++ src/cloud-tasks-queue-iam-policy/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/google/4.69.1/docs/resources/cloud_tasks_queue_iam_policy
 // generated from terraform resource schema
 
