8bc46fdfb25ebc4119dc5195b1b08120c94c8e04
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-07-11 03:19:18 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/kms-secret-ciphertext/index.ts
+++ src/kms-secret-ciphertext/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/google/4.73.0/docs/resources/kms_secret_ciphertext
 // generated from terraform resource schema
 
