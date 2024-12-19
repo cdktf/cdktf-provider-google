8166d2008c352c6645c277ac5f8b461697f7f6e0
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-12-19 14:51:06 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@7f995f5

--- src/edgenetwork-network/index.ts
+++ src/edgenetwork-network/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/google/6.14.1/docs/resources/edgenetwork_network
 // generated from terraform resource schema
 
