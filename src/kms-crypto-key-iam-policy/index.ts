0c4ce181ae653d61a846c9707dbb6344c999e514
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-12-22 06:12:56 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/kms-crypto-key-iam-policy/index.ts
+++ src/kms-crypto-key-iam-policy/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/google/6.14.1/docs/resources/kms_crypto_key_iam_policy
 // generated from terraform resource schema
 
