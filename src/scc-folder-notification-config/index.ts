dfa154884f88d1cb361a9b89fcf3f7e4ed7add3a
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-01-05 06:02:07 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@506334e

--- src/scc-folder-notification-config/index.ts
+++ src/scc-folder-notification-config/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/google/6.14.1/docs/resources/scc_folder_notification_config
 // generated from terraform resource schema
 
