4843a71ed5509224ab011a8f64f6fdb9f02e266d
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-06-03 15:49:22 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/logging-project-bucket-config/index.ts
+++ src/logging-project-bucket-config/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/google/6.37.0/docs/resources/logging_project_bucket_config
 // generated from terraform resource schema
 
